a93de5cb032356bff81feb43256c32e3f3f7a419
proyetei <75600096+proyetei@users.noreply.github.com>
2024-05-19 06:26:40 -0400

    add description of features

--- components/Features.tsx
+++ components/Features.tsx
@@ -37,11 +37,10 @@ export default function Features() {
             </AccordionTrigger>
             <AccordionContent>
               <p className="flex flex-col gap-6 text-xl text-slate-100">
-              hellos
+              FitFolio is an innovative full-stack application that allows users to provide their workout information such as the workout name, the reps and sets that they did and a title for the post.
                 <p className="">
-                  ...
+                Users can submit multiple workouts in one post. Users can also view workout entries on their dashboard, edit, delete and create as many workout entries they want. The workout information of each user then gets stored in the backend for the AI to analyze reports based on the user workout history.
                 </p>
-                ...
               </p>
             </AccordionContent>
           </AccordionItem>
@@ -61,11 +60,11 @@ export default function Features() {
             </AccordionTrigger>
             <AccordionContent>
               <p className="flex flex-col gap-6 text-lg text-slate-100">
-                ...
-                <p className="">
-                  ...
+              The AI compares all the workout history in the database, and then analyzes the texts to perform the following:
+                <p> - Critiques the fitness on their workout history and then gives suggestions for improvements.</p>
+                  <p> - Analyzes all the entries and texts of the workout names and its corresponding reps and set to tell the user what muscle they should workout more</p>
+                  <p> - Tells the user what workouts they should do more to target the specific muscles listed above
                 </p>
-                ...
               </p>
             </AccordionContent>
           </AccordionItem>
@@ -84,12 +83,10 @@ export default function Features() {
               </div>
             </AccordionTrigger>
             <AccordionContent>
-              <p className="flex flex-col gap-6 text-xl font-light text-slate-100">
-                ..
+              <p className="flex flex-col gap-6 text-xl font-light text-slate-100"> Allow users to ask the AI to generate a comprehensive workout plan for their next gym session. 
                 <p className="">
-                  ...
+                These reports can help users understand their progress, lead them to the right direction in their fitness journey by identifying areas for improvement, and maintain motivation by providing comprehensive workout routines.
                 </p>
-                ...
               </p>
             </AccordionContent>
           </AccordionItem>
